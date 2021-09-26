f2fdd428ddd4f1a028f16cdb157ea5e330db3a17
ebrah <ebraheemalshapi@gmail.com>
2021-09-26 01:14:14 -0400

    Remove unnecessary empty lines Step two finished

--- Python/Task1.py
+++ Python/Task1.py
@@ -1,14 +1,8 @@
-# %% md
-
 ## Step 1: Load the corpus using load files and make sure you set the encoding to latin1. (Task 1.3)
 
-# %% md
-
 ### Get and group the data
-
-# %%
-
 import sklearn.datasets
+import pandas as pd
 
 
 def load_files_of_bbc(category=None):
@@ -42,3 +36,31 @@ sportFiles, sportDataSize, sport_filenames = load_files_of_bbc('sport')
 
 # #BBC tech Data
 techFiles, techDataSize, tech_filenames = load_files_of_bbc('tech')
+
+## Step 2: Plot the distribution of the instances in each class and save the graphic in a file called BBC-distribution.pdf. (Task 1.2)
+
+### Create Dataframe
+
+row_name = 'Records Count'
+allBBC_DF = pd.DataFrame({
+    'Business': businessDataSize,
+    'Entertainment': entertainmentDataSize,
+    'Politics': politicsDataSize,
+    'Sport': sportDataSize,
+    'Tech': techDataSize
+},
+    index=[row_name]
+)
+
+print(allBBC_DF)
+
+### Plot the distribution of the instances in each class
+
+sp = allBBC_DF.loc[row_name].plot(by=allBBCFiles.target_names, title=row_name, figsize=(12, 6),
+                               kind='barh')
+
+### Save the graphic in a file called BBC-distribution.pdf
+
+fig = sp.get_figure()
+fig.savefig('../out/BBC-distribution.pdf')
+
